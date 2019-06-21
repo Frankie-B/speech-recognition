69127c104793ad988fb1887881f20fb455118ae7
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-06-21 22:44:01 +0300

    Turn on microphone and detect speech

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 // jshint esversion: 6
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  // The code below will detect speech
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+recognition.start();
+
