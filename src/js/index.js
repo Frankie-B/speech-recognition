1f6fbabf2ef80546b7eb739e00c09bc891b8441d
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-06-21 22:53:14 +0300

    Print out spoken words in dialogue box

--- src/js/index.js
+++ src/js/index.js
@@ -7,8 +7,25 @@ window.SpeechRecognition =
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
+// This code will create the paragraph elements in the HTML
 let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
+
+// This code will print out the spoken workds in the dialogue box
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+
+    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+    p.textContent = poopScript;
+
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+});
 
 recognition.start();
